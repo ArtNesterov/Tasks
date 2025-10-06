ca3d89d5945ec91264d10d49404a673feb450f9a
Артём Нестеров ТКИ-141 <artem.nestor15@mail.ru>
2025-10-06 11:36:35 +0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -21,14 +21,15 @@ double getArea(double radius, double angle);
 
 /**
  * @brief - точка входа
- * @return 0 если программа выполнена корректно 
+ * @return 0 если программа выполнена корректно
  */
 int main() {
 	setlocale(LC_ALL, "Russian");
-	double radius;
-	double angle;
+	double radius = 0;
+	double angle = 0;
 	printf("Введите радиус шара ");
 	scanf_s("%lf", &radius);
+
 	printf("Введите угол конуса при вершине ");
 	scanf_s("%lf", &angle);
 
@@ -40,10 +41,10 @@ int main() {
 
 double getVolume(const double radius, const double angle) {
 	double rad = angle * M_PI / 180; 
-	return (M_PI * pow(radius, 3) * pow(sin(rad), 2)*(1 + cos(rad))) / 3;
+	return (M_PI * pow(radius, 3) * pow(sin(rad), 2) * (1 + cos(rad))) / 3;
 }
 
-double getArea(const double radius,const double angle) {
+double getArea(const double radius, const double angle) {
 	double rad = angle * M_PI / 180;
 	return M_PI * pow(radius, 2) * pow(sin(rad), 2) + M_PI * (radius * sin(rad)) * (2 * radius * cos(rad / 2));
 }
